2383286285fa1d64b31c8c09958c435060150818
PhilipiGomes <philipi.rodrigues342@gmail.com>
2024-09-19 09:11:40 -0300

    Desafio Karython

--- PrimoOuComposto.py
+++ PrimoOuComposto.py
@@ -6,7 +6,7 @@ def divisores(n):
     i = 2  # Começa a verificar divisores a partir de 2
     while i < n:
         if n % i == 0:  # Se i divide n, adiciona à lista
-            divisors.insert(-1, i)  # Insere o divisor antes do último elemento (n)
+            divisors.insert(-1, i)  # Insere o divisor no penúltimo elemento
         i += 1
     return divisors
 
@@ -23,12 +23,10 @@ while n <= 1:
 
 # Obtém os divisores do número inserido
 divisors = divisores(n)
-os.system('cls')  # Limpa a tela antes de exibir o resultado
+os.system('cls')
 
 # Se houver apenas dois divisores (1 e n), o número é primo
 if len(divisors) == 2:
     print(f'{n} - primo')
 else:
-    # Converte os divisores em string e os exibe
-    divisores_string = ', '.join(str(i) for i in divisors)
-    print(f'{n} - composto, divisores: {divisores_string}')
+    print(f'{n} - composto, divisores: {divisors}')
